6f9a45c2259e7ebe5d1d90270249a58600e5c196
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-16 11:11:30 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+// Declare customerName to be 'bob' in global scope
+var customerName = 'bob';
+
+// Write a function that accesses the global customerName variable and uppercases it
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Write a function that overwrites the bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'new best customer';
+}
+
+// Declare a constant in global scope called leastFavoriteCustomer and assign it some initial value
+const leastFavoriteCustomer = 'someone';
+
+// Write a function that attempts to change the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    // Trying to reassign a constant will result in an error
+    // This line should throw an error
+    leastFavoriteCustomer = 'new least favorite';
+}
+
+module.exports = {
+    customerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    leastFavoriteCustomer,
+    changeLeastFavoriteCustomer,
+};
